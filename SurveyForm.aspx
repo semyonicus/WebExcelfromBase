c5c6efb6facfe65b1fde28e0e7f21d63d0457140
semyonicus <ss.mikhailov@s-vfu.ru>
2024-08-27 15:10:58 +0900

    Добавьте файлы проекта.

---
+++ SurveyForm.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SurveyForm.aspx.cs" Inherits="anketa.SurveyForm" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Выгрузка данных</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+   
+    <style>
+        .survey-form {
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 30px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            background-color: #f9f9f9;
+        }
+        .form-group {
+            position: relative;
+        }
+        .help-button {
+            display: inline-block;
+            width: 24px;
+            height: 24px;
+            border-radius: 50%;
+            background-color: #007bff;
+            color: white;
+            text-align: center;
+            line-height: 24px;
+            margin-left: 10px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            position: absolute;
+            right: 0;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+        .help-button:hover {
+            background-color: #0056b3;
+        }
+        .popover {
+            transition: opacity 0.3s;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('[data-toggle="popover"]').popover({
+                placement: 'bottom',
+                delay: {
+                    "show": 500,
+                    "hide": 100
+                }
+            });
+            $('[data-toggle="popover"]').click(function () {
+
+                setTimeout(function () {
+                    $('.popover').fadeOut('slow');
+                }, 3000);
+
+            });
+        });
+    </script>
+</head>
+<body>
+    <div class="container">
+        <form id="form1" runat="server" class="survey-form" enctype="multipart/form-data">
+            
+            <h2 class="text-center">
+                Выгрузка данных</h2>
+            <div class="form-group">
+              
+            <asp:ListBox ID="ListBoxConfigs" runat="server" 
+                AutoPostBack="true" 
+                OnSelectedIndexChanged="ListBoxConfigs_SelectedIndexChanged"
+                CssClass="form-control" style="width: 100%;"
+                ></asp:ListBox>
+            <asp:TextBox ID="QueryArea" runat="server" 
+                TextMode="MultiLine" Rows="10" Columns="50"
+                CssClass="form-control" style="width: 100%;"
+                ></asp:TextBox>
+            <span class="help-button" data-toggle="popover" data-content="Выгрузка данных может редактироваться прямо здесь">?</span>
+            </div>
+            
+            <asp:Button ID="SubmitButton" runat="server" Text="Загрузить данные" CssClass="btn btn-primary btn-block" OnClick="SubmitButton_Click" />
+        
+               </form>
+
+    </div>
+</body>
+</html>
---
+++ SurveyForm.aspx.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+//using Aspose.Cells;
+//using Aspose.Cells.Charts;
+using Spire.Xls;
+using Newtonsoft.Json.Linq;
+
+namespace anketa
+{
+    public partial class SurveyForm : Page
+    {
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            /*
+            string currentLogin = Context.User.Identity.Name;
+            if (string.IsNullOrEmpty(currentLogin))
+            {
+                // Обработка случая, когда пользователь не авторизован
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            var survey = new
+            {
+                Name = NameTextBox.Text,
+                Email = EmailTextBox.Text,
+                Comments = CommentsTextBox.Text
+            };
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonData = js.Serialize(survey);
+
+            string fileName = Path.GetFileName(FileUpload.PostedFile.FileName);
+            string filePath = Server.MapPath("~/excelfiles/") + fileName;
+
+            // Сохранение файла на сервере
+            FileUpload.SaveAs(filePath);
+
+            // Загрузка данных из Excel в таблицу AnketFiles
+            LoadExcelDataToDatabase(filePath);
+
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            string storedProcedure = "InsertSurveyData";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(storedProcedure, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@SurveyData", jsonData);
+                command.Parameters.AddWithValue("@FileName", fileName);
+                command.Parameters.AddWithValue("@UserName", currentLogin);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            */
+            string username = Context.User.Identity.Name;
+            if (string.IsNullOrEmpty(username))
+            {
+                Response.Write("надо зарегистрироваться от имени все зависит");
+                return;
+            }
+            string jsonString = QueryArea.Text;
+            if (jsonString=="нет данных")
+            {
+                Response.Write("у вас нет доступа на эту операцию");
+                return;
+            }
+            string filename;
+            Workbook workbook = new Workbook();
+            // в зависимости от выбора должно быть
+            filename = Server.MapPath("~/www/rasp/target.xlsx");
+
+                if (!File.Exists(filename))
+                {
+                    Response.Write("Ошибка шаблон не найден");
+                    return;
+                
+                }
+                else
+                {
+                    workbook.LoadFromFile(filename);
+                }
+                JArray json;
+            try
+            {
+                json = JArray.Parse(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Ошибка парсинга JSON: " + ex.Message + "Пример правильного содержания:" +
+                    "[{\"procedure\":\"publicbase.dbo.GetStudentsbyDate\"," +
+                    "\"params\":[{\"param\":\"date\", \"value\":\"20240401\"," +
+                    "\"type\":\"string\"},{\"param\":\"trouble\", \"value\":\"0\",\"type\":\"int\"}],\"sheetnum\":2}," +
+                    "{\"procedure\":\"publicbase.dbo.GetStudentsbyDate\",\"param\":\"date\", \"value\":\"20240101\"," +
+                    "\"type\":\"string\",\"sheetnum\":3}]");
+                return;
+            }
+            string connectionString=ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch
+                {
+                    Response.Write("ошибка учетных данных");
+                    return;
+                }
+                string paramName;
+                string paramValue;
+                string paramType;
+                string errors;
+                // Обработка каждой хранимой процедуры в JSON
+                foreach (var query in json)
+                {
+                    string procedureName = query["procedure"].ToString();
+                    int sheetNum = (int)query["sheetnum"];
+                    // Создание команды для вызова хранимой процедуры
+                    using (SqlCommand command = new SqlCommand(procedureName, connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        paramName = "";
+                        paramValue = "";
+                        paramValue = "";
+                        errors = "";
+                        foreach (var param in query["params"])
+                        {
+                            paramName = param["param"].ToString();
+                            paramValue = param["value"].ToString();
+                            paramType = param["type"].ToString();
+
+                            // Проверка наличия обязательных параметров
+                            if (string.IsNullOrEmpty(procedureName) || string.IsNullOrEmpty(paramName) || string.IsNullOrEmpty(paramValue) || string.IsNullOrEmpty(paramType) || sheetNum <= 0)
+                            {
+                                if (string.IsNullOrEmpty(procedureName))
+                                {
+                                    errors += "Отсутствует обязательный параметр имя Процедуры в процедуре. ";
+                                }
+                                if (string.IsNullOrEmpty(paramName))
+                                {
+                                    errors += "Отсутствует обязательный параметр имя параметра - param.  ";
+                                }
+                                if (string.IsNullOrEmpty(paramValue))
+                                {
+                                    errors += "Отсутствует обязательный параметр значение параметра - value. ";
+                                }
+                                if (string.IsNullOrEmpty(paramType))
+                                {
+                                    errors += "Отсутствует обязательный параметр тип параметра - type, должен быть либо string либо int ";
+                                }
+                                if (sheetNum <= 0)
+                                {
+                                    errors += "Отсутствует обязательный параметр номер листа ";
+                                }
+
+                                errors += "Пример правильного содержания ячейки A1:" +
+                                "[{\"procedure\":\"publicbase.dbo.GetStudentsbyDate\"," +
+                                "\"params\":[{\"param\":\"date\", \"value\":\"20240401\"," +
+                                "\"type\":\"string\"},{\"param\":\"trouble\", \"value\":\"0\",\"type\":\"int\"}],\"sheetnum\":2}," +
+                                "{\"procedure\":\"publicbase.dbo.GetStudentsbyDate\",\"param\":\"date\", \"value\":\"20240101\"," +
+                                "\"type\":\"string\",\"sheetnum\":3}]";
+
+                                Response.Write("есть пустые значения" + errors);
+                                return;
+                                // код поставлю из другого проекта
+                            }
+                            SqlParameter sqlParam = new SqlParameter(paramName, GetSqlDbType(paramType))
+                            {
+                                Value = paramValue
+                            };
+                            command.Parameters.Add(sqlParam);
+                        }
+                        paramName = "username";
+                        paramType = "string";
+                        SqlParameter sqlUserName = new SqlParameter(paramName, GetSqlDbType(paramType))
+                        {
+                            Value = username
+                        };
+                        command.Parameters.Add(sqlUserName);
+
+                        // Проверка существования листа и создание, если его нет
+                        Worksheet resultSheet;
+                        if (sheetNum > workbook.Worksheets.Count)
+                        {
+                            resultSheet = workbook.Worksheets.Add($"Sheet{sheetNum}");
+                        }
+                        else
+                        {
+                            resultSheet = workbook.Worksheets[sheetNum - 1];
+                        }
+
+                        try
+                        {
+                            // Выполнение команды и получение результатов
+                            using (SqlDataReader reader = command.ExecuteReader())
+                            {
+                                int row = 1;
+
+                                // Запись наименований полей в первую строку
+                                for (int col = 0; col < reader.FieldCount; col++)
+                                {
+                                    resultSheet.Range[row, col + 1].Text = reader.GetName(col);
+                                }
+                                row++;
+                                int currentRow = 0;
+                                while (reader.Read())
+                                {
+                                    for (int col = 0; col < reader.FieldCount; col++)
+                                    {
+                                        resultSheet.Range[row, col + 1].Text = reader[col].ToString();
+                                    }
+                                    row++;
+                                    currentRow++;
+                
+                                }
+                            }
+
+                        }
+                        catch (SqlException ex)
+                        {
+                            Response.Write($"Извините, хранимой процедуры {procedureName} нет или у вас нет доступа: {ex.Message}");
+                            return;
+                        }
+                    }
+                }
+
+            }
+            // string resultFilePath = "result.xlsx";
+            // resultFilePath = Server.MapPath("~/www/rasp/result.xlsx");
+
+            // Сохранение изменений в файл Excel
+            //workbook.SaveToFile(resultFilePath);
+
+            // Отправка файла клиенту
+            //Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            //Response.AppendHeader("Content-Disposition", "attachment; filename=result.xlsx");
+            //Response.TransmitFile(resultFilePath);
+            //Response.End();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                workbook.SaveToStream(stream, FileFormat.Version2013);
+                stream.Position = 0;
+
+                // Отправка файла клиенту
+                Response.Clear();
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("Content-Disposition", "attachment; filename=result.xlsx");
+                Response.BinaryWrite(stream.ToArray());
+                Response.End();
+            }
+        }
+        /*
+        private void LoadExcelDataToDatabase(string filePath)
+        {
+            Workbook workbook = new Workbook(filePath);
+            Worksheet worksheet = workbook.Worksheets[0];
+            DataTable dataTable = worksheet.Cells.ExportDataTable(0, 0, worksheet.Cells.MaxDataRow + 1, worksheet.Cells.MaxDataColumn + 1, true);
+
+            // ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString тянет из web конфига
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlBulkCopy bulkCopy = new SqlBulkCopy(connection);
+                bulkCopy.DestinationTableName = "AnketFiles";
+                connection.Open();
+                bulkCopy.WriteToServer(dataTable);
+                connection.Close();
+            }
+
+            // Вызов другой хранимой процедуры для дальнейшей обработки данных
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("ProcessAnketFiles", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }*/
+        private JObject configJson;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string filePath = Server.MapPath("~/www/rasp/config.json");
+                string jsonContent = File.ReadAllText(filePath);
+                configJson = JObject.Parse(jsonContent);
+
+                if (configJson["version"] == null)
+                {
+                    Response.Write("Неверный формат JSON файла: отсутствует ключ 'version'.");
+                    return;
+                }
+                ListBoxConfigs.Items.Clear();
+                foreach (var config in configJson["configs"])
+                {
+                    ListBoxConfigs.Items.Add(config["name"].ToString());
+                }
+            }
+
+        }
+        protected void ListBoxConfigs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (ListBoxConfigs.SelectedItem != null)
+            {
+                string filePath = Server.MapPath("~/www/rasp/config.json");
+                string jsonContent = File.ReadAllText(filePath);
+                configJson = JObject.Parse(jsonContent);
+
+                string selectedConfigName = ListBoxConfigs.SelectedItem.ToString();
+                var selectedConfig = configJson["configs"].First(config => config["name"].ToString() == selectedConfigName);
+                string username = Context.User.Identity.Name;
+                Response.Write(username);
+ //               username = "mi.maht1";
+                int facValue = GetFacValue(username);
+                if (facValue > 0)
+                {
+                    string configData = selectedConfig["data"].ToString();
+                    configData = configData.Replace("%fac%", facValue.ToString());
+                    QueryArea.Text = configData;
+                } else
+                {
+                    QueryArea.Text = "нет данных";
+                }
+            }
+        }
+
+        private int GetFacValue(string username)
+        {
+            int facValue = 0;
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("publicbase.dbo.getfac", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add(new SqlParameter("@username", username));
+
+                    connection.Open();
+                    facValue = (int)command.ExecuteScalar();
+                }
+            }
+
+            return facValue;
+        }
+        static SqlDbType GetSqlDbType(string type)
+        {
+            switch (type.ToLower())
+            {
+                case "int":
+                    return SqlDbType.Int;
+                case "string":
+                    return SqlDbType.NVarChar;
+                default:
+                    throw new ArgumentException("Unsupported type");
+            }
+        }
+
+    }
+}
